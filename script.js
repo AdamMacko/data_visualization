921555785cee102603e2173c1bfee643c95f791c
AdamMacko <mackoadam60@gmail.com>
2025-02-15 21:48:09 +0100

    animacia trasy

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const zoomGroup = svg.select("#zoomGroup"); // obsah svg okna
 const mapContainer = document.getElementById("mapContainer");
 const editPointMenu = document.getElementById("edit-point-menu"); // Rozbalovacie menu pre upravu bodu
 const sizeSlider = document.getElementById("sizeSlider"); // slider pre zmenu velkosti
+const pathGroup = zoomGroup.append("g").attr("id", "pathGroup"); // Skupina pre trasu
+const pointsGroup = zoomGroup.append("g").attr("id", "pointsGroup"); // Skupina pre body
+
 
 // ---- Ovladacie prvky ----
 const buttons = {
@@ -31,6 +34,10 @@ let isAddingPoint = false;
 let isDeletingPoint = false;
 let pointCounter = 1; // premenna pre ocislovanie pridaneho bodu
 let selectedPoint = null; // aktualne vybrany bod
+let animationRunning = false;//pomocna boolean pre animovane vykreslovanie
+let animationIndex = 0; // index na ktorom bode animujeme aktualne
+let movingCircle = null; // kruh animacie, ktory prechadza trasou
+let pathLine = null; // hrany medzi bodmi
 
 
 // rozbalovacie menu funkcionalita
@@ -61,20 +68,32 @@ function resetZoom() {
     svg.transition().call(zoom.transform, d3.zoomIdentity);
     currentTransform = d3.zoomIdentity;
 }
-
-// pridavanie bodov
+//pridavanie bodov button
 buttons.add.addEventListener("click", () => {
     isAddingPoint = !isAddingPoint;
-    buttons.add.textContent = isAddingPoint ? "Zrušiť pridávanie bodov" : "Pridať bod";
+    
+    if (isAddingPoint) {
+        buttons.add.innerHTML = `<span class="material-symbols-outlined">close</span> Zrušiť pridávanie bodov`;
+    } else {
+        buttons.add.innerHTML = `<span class="material-symbols-outlined">add</span> Pridať bod`;
+    }
+
     buttons.add.classList.toggle("active", isAddingPoint);
 });
+
 
 // odstranovanie bodov
 buttons.delete.addEventListener("click", () => {
     isDeletingPoint = !isDeletingPoint;
-    buttons.delete.textContent = isDeletingPoint ? "Zrušiť odstraňovanie" : "Odstrániť bod";
+    
+
+    buttons.delete.innerHTML = isDeletingPoint 
+        ? `<span class="material-symbols-outlined">cancel</span> Zrušiť odstraňovanie` 
+        : `<span class="material-symbols-outlined">delete</span> Odstrániť bod`;
+
     buttons.delete.classList.toggle("active", isDeletingPoint);
 });
+
 
 // kliknutie na SVG + pridavanie bodov
 svg.on("click", (event) => {
@@ -91,8 +110,8 @@ function addPoint(x, y) {
     const point = { x, y, id: `C${pointCounter}` };
     points.push(point);
 
-    // Pridanie bodu do svg
-    const circle = zoomGroup.append("circle")
+    // Pridanie bodu do SVG
+    const circle = pointsGroup.append("circle")
         .attr("cx", x)
         .attr("cy", y)
         .attr("r", 5)
@@ -108,11 +127,11 @@ function addPoint(x, y) {
             }
         });
 
-    // suradnice pre text (cislo bodu)
+    // suradnice pre text- cislovanie bodov
     const textX = x - 3.5;
     const textY = y + 4;
 
-    const text = zoomGroup.append("text")
+    const text = pointsGroup.append("text")
         .attr("x", textX)
         .attr("y", textY)
         .text(pointCounter)
@@ -126,30 +145,55 @@ function addPoint(x, y) {
 
     console.log(`Pridaný bod: ${point.id} (x=${x}, y=${y})`);
     pointCounter++;
+
     isAddingPoint = false;
-    buttons.add.textContent = "Pridať bod";
+    buttons.add.innerHTML = `<span class="material-symbols-outlined">add</span> Pridať bod`;
     buttons.add.classList.remove("active");
 }
 
-// funkcia pre vymazanie bodu 
-function removePoint(element, point) {
-    const index = points.findIndex(p => p.x === point.x && p.y === point.y);
-    if (index !== -1) points.splice(index, 1);
 
+// funkcia na odstranenie bodu- cislovanie
+function removePoint(element, point) {
+    // Nájdeme index bodu
+    const index = points.findIndex(p => p.id === point.id);
+    if (index !== -1) {
+        points.splice(index, 1);
+    }
+
+    
     point.circleElement.remove();
     point.textElement.remove();
 
     const textIndex = textCoordinates.findIndex(coord => coord.x === point.textElement.attr("x") && coord.y === point.textElement.attr("y"));
-    if (textIndex !== -1) textCoordinates.splice(textIndex, 1);
+    if (textIndex !== -1) {
+        textCoordinates.splice(textIndex, 1);
+    }
 
-    pointCounter--;
     console.log(`Odstránený bod: ${point.id}`);
+
+
+    updatePointNumbers();
+
+    isDeletingPoint = false;
+    buttons.delete.innerHTML = `<span class="material-symbols-outlined">delete</span> Odstrániť bod`;
+    buttons.delete.classList.remove("active");
 }
+
+function updatePointNumbers() {
+    points.forEach((point, index) => {
+        point.textElement.text(index + 1); 
+        point.id = `C${index + 1}`;
+    });
+
+    pointCounter = points.length + 1;
+}
+
+
 
 // funkcia na vyber bodu + pulzovanie
 function selectPoint(element, point) {
 
-    zoomGroup.selectAll("circle").classed("pulsating", false);
+    pointsGroup.selectAll("circle").classed("pulsating", false);
 
     if (selectedPoint !== point) {
         selectedPoint = point;
@@ -182,7 +226,7 @@ function openColorPicker() {
     }
     
     const colorPicker = document.getElementById("colorPicker");
-    colorPicker.click();  // Otvori rgb
+    colorPicker.click();  // otvori rgb
 
     colorPicker.addEventListener("input", function handler(event) {
         const newColor = event.target.value;
@@ -209,3 +253,111 @@ function openSizeInput() {
     sizeSlider.value = currentSize;
     sizeSlider.style.display = "block";
 }
+
+
+
+
+function play(duration = 3000) {
+    if (points.length < 2 || animationRunning) return;
+    animationRunning = true;
+
+    // ak animacia uz bezi, nerob nic
+    if (!movingCircle) {
+        movingCircle = pathGroup.append("circle")
+            .attr("cx", points[animationIndex].x)
+            .attr("cy", points[animationIndex].y)
+            .attr("r", 6)
+            .attr("fill", "green");
+    }
+
+    // Pri kazdom spusteni zresetujeme trasu
+    if (pathLine) {
+        pathLine.remove();
+    }
+    pathLine = pathGroup.append("path")
+        .attr("fill", "none")
+        .attr("stroke", "blue")
+        .attr("stroke-width", 2)
+        .attr("d", `M ${points[animationIndex].x} ${points[animationIndex].y}`);
+
+    function moveToNextPoint(index) {
+        if (!animationRunning || index >= points.length) {
+            animationRunning = false;
+            return;
+        }
+        animationIndex = index; // ulozime aktualny bod
+
+        movingCircle.transition()
+            .duration(duration)
+            .attr("cx", points[index].x)
+            .attr("cy", points[index].y)
+            .on("end", () => moveToNextPoint(index + 1));
+
+        let currentPath = pathLine.attr("d");
+        pathLine.attr("d", currentPath + ` L ${points[index].x} ${points[index].y}`);
+    }
+
+    moveToNextPoint(animationIndex + 1);
+}
+
+
+
+
+function stop() {
+    animationRunning = false;
+    if (movingCircle) {
+        movingCircle.interrupt();
+        animationIndex--;
+    }
+}
+
+function stepBack() {
+    if (animationIndex > 0) {
+        animationIndex--;
+        movingCircle.attr("cx", points[animationIndex].x)
+                    .attr("cy", points[animationIndex].y);
+    }
+}
+
+function stepForward() {
+    if (animationIndex <= points.length) {
+        animationIndex++;
+        movingCircle.attr("cx", points[animationIndex].x)
+                    .attr("cy", points[animationIndex].y);
+    }
+}
+
+function resetWholeMap() {
+
+    pointsGroup.selectAll("circle").remove();
+    pointsGroup.selectAll("text").remove();
+
+
+    pathGroup.selectAll("*").remove();
+
+    points = [];
+    textCoordinates = [];
+    pointCounter = 1;
+    selectedPoint = null;
+
+    resetZoom();
+
+
+    animationRunning = false;
+    animationIndex = 0;
+
+
+    if (movingCircle) {
+        movingCircle.remove();
+        movingCircle = null;
+    }
+
+    if (pathLine) {
+        pathLine.remove();
+        pathLine = null;
+    }
+
+    console.log("Mapa bola resetovaná.");
+}
+
+
